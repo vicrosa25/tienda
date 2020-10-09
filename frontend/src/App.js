844ef714878d6db283d7c2bc22f4d5d39b56fcdf
Victor Rosa <vicrosa25@gmail.com>
2020-10-09 10:59:12 +0200

    Crea el Home y el componente producto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Home from './screens/Home'
 import "./App.css";
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Bienvenidos a Joyería Reina</h1>
+          <Home />
         </Container>
       </main>
       <Footer />
