90ee83a7270750d46950dd91a6ee0e9c44f690e8
Victor Rosa <vicrosa25@gmail.com>
2020-10-09 11:48:35 +0200

    Componente Rating hecho

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Home from './screens/Home'
+import Home from "./screens/Home";
 import "./App.css";
 
 function App() {
