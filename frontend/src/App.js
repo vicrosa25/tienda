247f5b64015734b9da3f07b4f112ec8c81760499
Victor Rosa <vicrosa25@gmail.com>
2020-10-09 12:23:49 +0200

    Implementa React Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./screens/Home";
+import ProductScreen from "./screens/ProductScreen";
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <Home />
+          <Route path="/" component={Home} exact />
+          <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
